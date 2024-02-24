21f9e5ed24e481f79c8c70aa1c00421e62b546c3
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-02-24 16:20:41 +0800

    Update Get-WinDisplayLanguage.ps1

--- src/functions/Get-WinDisplayLanguage.ps1
+++ src/functions/Get-WinDisplayLanguage.ps1
@@ -2,28 +2,32 @@ $LANGUAGE_REG_PATH = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Cont
 $LANGUAGE_REG_KEY_DEFAULT = "Default"
 $LANGUAGE_REG_KEY_INSTALL = "InstallLanguage"
 
-function Edit-Registry($Lang) {
+function Edit-Registry {
   Set-ItemProperty -Path $LANGUAGE_REG_PATH `
                    -Name $LANGUAGE_REG_KEY_DEFAULT `
-                   -Value $Lang.HexValue
+                   -Value $Script:SELECTED_LANG.HexValue
   Set-ItemProperty -Path $LANGUAGE_REG_PATH `
                    -Name $LANGUAGE_REG_KEY_INSTALL `
-                   -Value $Lang.HexValue
+                   -Value $Script:SELECTED_LANG.HexValue
 }
 
-function Get-LanguagePack($Lang) {
-  $DownloadJob = Install-Language -Language $Lang.LanguageCode -CopyToSettings -AsJob
+function Get-LanguagePack {
+  $DownloadJob = Install-Language -Language $Script:SELECTED_LANG.LanguageCode -CopyToSettings -AsJob
 
   while ($DownloadJob.State -eq 'Running') {
-    Write-Host -NoNewLine "`rInstalling $($Lang.Name) *.... " -ForegroundColor Cyan
+    Write-Host -NoNewLine "`rInstalling $($Script:SELECTED_LANG.Name) [.....] " -ForegroundColor Cyan
     Start-Sleep -Milliseconds 100
-    Write-Host -NoNewLine "`rInstalling $($Lang.Name) .*... " -ForegroundColor Cyan
+    Write-Host -NoNewLine "`rInstalling $($Script:SELECTED_LANG.Name) [*....] " -ForegroundColor Cyan
     Start-Sleep -Milliseconds 100
-    Write-Host -NoNewLine "`rInstalling $($Lang.Name) ..*.. " -ForegroundColor Cyan
+    Write-Host -NoNewLine "`rInstalling $($Script:SELECTED_LANG.Name) [.*...] " -ForegroundColor Cyan
     Start-Sleep -Milliseconds 100
-    Write-Host -NoNewLine "`rInstalling $($Lang.Name) ...*. " -ForegroundColor Cyan
+    Write-Host -NoNewLine "`rInstalling $($Script:SELECTED_LANG.Name) [..*..] " -ForegroundColor Cyan
     Start-Sleep -Milliseconds 100
-    Write-Host -NoNewLine "`rInstalling $($Lang.Name) ....* " -ForegroundColor Cyan
+    Write-Host -NoNewLine "`rInstalling $($Script:SELECTED_LANG.Name) [...*.] " -ForegroundColor Cyan
+    Start-Sleep -Milliseconds 100
+    Write-Host -NoNewLine "`rInstalling $($Script:SELECTED_LANG.Name) [....*] " -ForegroundColor Cyan
+    Start-Sleep -Milliseconds 100
+    Write-Host -NoNewLine "`rInstalling $($Script:SELECTED_LANG.Name) [.....] " -ForegroundColor Cyan
     Start-Sleep -Milliseconds 100
   }
 
@@ -31,38 +35,21 @@ function Get-LanguagePack($Lang) {
     'Completed' {
       Wait-Job $DownloadJob >$null 2>&1
       Remove-Job $DownloadJob
-      Write-Host "`rInstalled $($Lang.Name)" -ForegroundColor Green
+      Write-Host "`rInstalled $($Script:SELECTED_LANG.Name)" -ForegroundColor Green
     }
-    'Failed' { Write-Host "`r:: Download encountered an error.`n" -ForegroundColor DarkYellow; Pause }
-    'Stopped' { Write-Host "`r:: Download was manually stopped.`n" -ForegroundColor DarkYellow; Pause }
+    'Failed' { Write-Host "`n:: Download encountered an error.`n" -ForegroundColor Yellow; Pause; exit }
+    'Stopped' { Write-Host "`n:: Download was manually stopped.`n" -ForegroundColor Yellow; Pause; exit }
   }
 }
 
-function Update-LanguageList($Lang) {
-  $LangList = Get-WinUserLanguageList
-  $LangList.Insert(0, $Lang.LanguageCode)
-  Set-WinUserLanguageList $LangList
-  # Set-WinUserLanguageList -LanguageList $Lang.LanguageCode -Force
+function Update-LanguageList {
+  $LanguageList = Get-WinUserLanguageList
+  $LanguageList.Insert(0, $Script:SELECTED_LANG.LanguageCode)
+  Set-WinUserLanguageList $LanguageList
+  # Set-WinUserLanguageList -LanguageList $Script:SELECTED_LANG.LanguageCode -Force
 }
 
-function Set-SystemUILanguage($Lang) {
-  Set-WinSystemLocale $Lang.LanguageCode
-  Set-SystemPreferredUILanguage -Language $Lang.LanguageCode
+function Set-SystemUILanguage {
+  Set-WinSystemLocale $Script:SELECTED_LANG.LanguageCode
+  Set-SystemPreferredUILanguage -Language $Script:SELECTED_LANG.LanguageCode
 }
-
-# Uninstall-Language -Language $SelectedLanguage.LanguageCode
-
-<#
-Get-InstalledLanguage | ForEach-Object { 
-  if ($_.Language -eq $SelectedLanguage.LanguageCode) {
-    Write-Host $SelectedLanguage.Name is already installed.
-  } else { 
-    Set-ItemProperty -Path $LANGUAGE_REG_PATH -Name $LANGUAGE_REG_KEY -Value $SelectedLanguage.HexValue
-    Write-Host Installed $SelectedLanguage.Name
-    # Update language list
-    # $LangList = Get-WinUserLanguageList
-    # $LangList.Add($SelectedLanguage.LanguageCode)
-    # Set-WinUserLanguageList -LanguageList $LangList
-  }
-}
-#>

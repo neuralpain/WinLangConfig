048bd972079cf33b8fbb1de88f8e05d94513e66a
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-02-24 16:20:54 +0800

    Update Main.ps1

--- src/Main.ps1
+++ src/Main.ps1
@@ -3,11 +3,19 @@ Write-Host "`n--- CURRENT DISPLAY LANGUAGE ---" -ForegroundColor White
 DISM /online /Get-Intl
 Write-Host "`n--- END CURRENT DISPLAY LANGUAGE ---`n" -ForegroundColor White
 
-$SelectedLanguage = $Lang_French
-Write-Registry $SelectedLanguage
-Add-Language $SelectedLanguage
-Update-LanguageList $SelectedLanguage
-Set-UiLang $SelectedLanguage
+Write-Host "TOTAL AVAILABLE LANGUAGES: $($LanguageObjectList.Length)`n"
+
+$i = 0
+foreach ($lang in $LanguageObjectList) {
+  Write-Host $i $lang.Name; $i++
+}
+
+Write-Menu
+# $Script:SELECTED_LANG = $Lang_Chinese
+Edit-Registry
+Get-LanguagePack
+Update-LanguageList
+Set-SystemUILanguage
 
 # show language packs info
 Write-Host "`n--- NEW DISPLAY LANGUAGE ---" -ForegroundColor White
@@ -17,3 +25,4 @@ Write-Host "`n--- END NEW DISPLAY LANGUAGE ---`n" -ForegroundColor White
 Write-Host "Please restart your Windows Operating System for the changes to take effect." -ForegroundColor Yellow
 
 Pause
+exit
